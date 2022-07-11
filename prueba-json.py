fa243107d6444fd4a15f61b52a25c93a17f871d5
Diego Suárez Martínez <dsuarez@intermarkit.es>
2022-07-11 11:00:03 +0200

    Nuevos archivos

---
+++ prueba-json.py
@@ -0,0 +1,65 @@
+from fileinput import close
+import json
+import os
+
+
+
+f = "mijson.json"
+mi_diccionario= json.load(open('mijson.json'))
+
+
+#opciones
+
+print("1. Mostrar los servidores")
+print("2. Agregar servidor")
+print("3. Eliminar servidor")
+
+pregunta = input("\nElige una opcion:")
+
+
+if pregunta == "1":
+
+    print ("\n")
+    for key in mi_diccionario:
+        print (key)
+    
+    servidores = input("\nIntroduce el nombre del servidor al que quieres conectarte: ")
+    usuario= mi_diccionario[servidores]['user']
+    conexion= mi_diccionario[servidores]['ip']
+       
+    cmd = ' ssh ' + usuario  + "@" + conexion
+
+    os.system(cmd)
+
+elif pregunta == "2":
+
+    usuario= input("Introduce el nuevo usuario: ")
+    servidor= input("Introduce el nombre del servidor: ")
+    ip= input("Introduce la ip de conexion: ")
+
+    nuevo_servidor = {
+        "user": usuario,
+        "ip": ip
+    }
+
+    mi_diccionario[servidor] = nuevo_servidor
+
+    json.dump (mi_diccionario, open('mijson.json', 'w'), indent = 4)
+
+elif pregunta == "3":
+    del_servidor = input("Introduce el nombre del servidor que quieras borrar: ")
+    mi_diccionario.pop(del_servidor)
+
+    json.dump (mi_diccionario, open('mijson.json', 'w'), indent = 4)
+
+    
+
+else:
+    print("Esa no es una opción valida")
+
+
+
+
+
+
+
